99b70017fd85ba47b66ef619e7b7073fc6438e77
Pierce <pierceevans@me.com>
2022-12-27 17:50:25 -0600

    commit tree 34100ce42ae47343890af44aa18b4bc701f01422

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
 // Code your solution here
+//const drivers = ['Bobby', 'Sammy', 'Sally', 'Annette', 'Sarah', 'Bobby'].toLowerCase();
+
+function findMatching(drivers, name){
+
+        return drivers.filter(element => element.toLowerCase() === name.toLowerCase())
+     }
+
+function fuzzyMatch(drivers, name){
+    return drivers.filter(element => element.slice(0,name.length).toLowerCase() === name.toLowerCase())
+}
+function matchName(drivers, name){
+    return drivers.filter(element => element.name.toLowerCase() === name.toLowerCase())
+}
